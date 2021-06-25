badf78ff318f02ddfac799200bd821ace829d76e
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-06-25 18:37:48 +0200

    Create example information object

---
+++ src/constants/videos-mock.js
@@ -0,0 +1,16 @@
+const VIDEO = [
+  {
+    id: 1,
+    name: 'Adidas x FC Bayern',
+    client: 'ADIDAS',
+    link: 'https://player.vimeo.com/video/447083641?badge=0&autopause=0&player_id=0&app_id=58479'
+  },
+  {
+    id: 2,
+    name: 'Emma One TVC',
+    client: 'EMMA Agency',
+    link: 'https://player.vimeo.com/video/461749664?badge=0&autopause=0&player_id=0&app_id=58479'
+  }
+];
+
+export default VIDEO;
