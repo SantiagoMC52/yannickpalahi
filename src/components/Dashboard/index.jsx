4bc09b2d49ff8bde874c839dff2149d279323a0a
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-06-25 21:09:50 +0200

    Add data to dashboard

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { loadInfo } from '../../redux/actions/actionCreators';
 
-const Dashboard = () => (
-  <h2>Yannick Palahí</h2>
-);
+const Dashboard = () => {
+  const info = useSelector((store) => store.info);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!info.lenght) dispatch(loadInfo());
+  });
+
+  return (
+    <>
+      <ul>
+        {
+          info.map((detail) => (
+            <Link to={`/detail/${detail.id}`}>
+              <li key={detail.id}>{detail.name}</li>
+            </Link>
+          ))
+        }
+      </ul>
+    </>
+  );
+};
 export default Dashboard;
