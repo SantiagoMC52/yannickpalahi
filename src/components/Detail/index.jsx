2b9f8d1a2c886a5774a0f03358a8383376497bc5
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-06-29 19:36:24 +0200

    Refactor code in detail component

--- src/components/Detail/index.jsx
+++ src/components/Detail/index.jsx
@@ -20,171 +20,173 @@ const Detail = () => {
       <div className="main-container__title">
         <h2>{selectedProject?.title}</h2>
       </div>
-      {selectedProject?.link
-        ? (
-          <iframe
-            className="main-container__frame"
-            src={selectedProject?.link}
-            frameBorder="0"
-            allowFullScreen
-          />
-        ) : ''}
-      <div className="main-container__extra">
-        <span>{selectedProject?.type}</span>
-        <span>{selectedProject?.year}</span>
+      <div className="main-container-data">
+        {selectedProject?.link
+          ? (
+            <iframe
+              className="main-container-data__frame"
+              src={selectedProject?.link}
+              frameBorder="0"
+              allowFullScreen
+            />
+          ) : ''}
+        <div className="main-container-data__extra">
+          <span>{selectedProject?.type}</span>
+          <span>{selectedProject?.year}</span>
+        </div>
+        <div className="main-container-data__description">
+          {selectedProject?.description
+            ? (
+              <>
+                {selectedProject?.description}
+              </>
+            ) : ''}
+        </div>
+        <ul className="main-container-data__ul">
+          {selectedProject?.producer
+            ? (
+              <li>
+                Written, directed and produced by
+                {' '}
+                {selectedProject?.producer}
+              </li>
+            ) : '' }
+          {selectedProject?.creator
+            ? (
+              <li>
+                .Futuro is
+                {' '}
+                {selectedProject?.creator}
+              </li>
+            ) : ''}
+          {selectedProject?.dop
+            ? (
+              <li>
+                DOP,
+                {' '}
+                {selectedProject?.dop}
+              </li>
+            ) : ''}
+          {selectedProject?.artDirection
+            ? (
+              <li>
+                Art direction,
+                {' '}
+                {selectedProject?.artDirection}
+              </li>
+            ) : ''}
+          {selectedProject?.focus
+            ? (
+              <li>
+                Focus puller,
+                {' '}
+                {selectedProject?.focus}
+              </li>
+            ) : ''}
+          {selectedProject?.camAssistant
+            ? (
+              <li>
+                Camera assistant,
+                {' '}
+                {selectedProject?.camAssistant}
+              </li>
+            ) : ''}
+          {selectedProject?.artAssistant
+            ? (
+              <li>
+                Art assistant,
+                {' '}
+                {selectedProject?.artAssistant}
+              </li>
+            ) : ''}
+          {selectedProject?.producer
+            ? (
+              <li>
+                Production,
+                {' '}
+                {selectedProject?.producer}
+              </li>
+            ) : ''}
+          {selectedProject?.productionAssistant
+            ? (
+              <li>
+                Production assistant,
+                {' '}
+                {selectedProject?.productionAssistant}
+              </li>
+            ) : ''}
+          {selectedProject?.car
+            ? (
+              <li>
+                Car by,
+                {' '}
+                {selectedProject?.car}
+              </li>
+            ) : ''}
+          {selectedProject?.music
+            ? (
+              <li>
+                Musci,
+                {' '}
+                {selectedProject?.music}
+              </li>
+            ) : ''}
+          {selectedProject?.color
+            ? (
+              <li>
+                Color grading,
+                {' '}
+                {selectedProject?.color}
+              </li>
+            ) : ''}
+          {selectedProject?.graphicDesigner
+            ? (
+              <li>
+                Graphic designer,
+                {' '}
+                {selectedProject?.graphicDesigner}
+              </li>
+            ) : ''}
+          {selectedProject?.compositing
+            ? (
+              <li>
+                Compositing,
+                {' '}
+                {selectedProject?.compositing}
+              </li>
+            ) : ''}
+          {selectedProject?.matte
+            ? (
+              <li>
+                Matte Painting,
+                {' '}
+                {selectedProject?.matte}
+              </li>
+            ) : ''}
+          {selectedProject?.editing
+            ? (
+              <li>
+                Editing,
+                {' '}
+                {selectedProject?.editing}
+              </li>
+            ) : ''}
+          {selectedProject?.artSupplier
+            ? (
+              <li>
+                Production assistant,
+                {' '}
+                {selectedProject?.artSupplier}
+              </li>
+            ) : ''}
+          {selectedProject?.coming
+            ? (
+              <li>
+                {selectedProject?.coming}
+              </li>
+            ) : ''}
+        </ul>
       </div>
-      <div className="main-container__description">
-        {selectedProject?.description
-          ? (
-            <>
-              {selectedProject?.description}
-            </>
-          ) : ''}
-      </div>
-      <ul className="main-container__ul">
-        {selectedProject?.producer
-          ? (
-            <li>
-              Written, directed and produced by
-              {' '}
-              {selectedProject?.producer}
-            </li>
-          ) : '' }
-        {selectedProject?.creator
-          ? (
-            <li>
-              .Futuro is
-              {' '}
-              {selectedProject?.creator}
-            </li>
-          ) : ''}
-        {selectedProject?.dop
-          ? (
-            <li>
-              DOP,
-              {' '}
-              {selectedProject?.dop}
-            </li>
-          ) : ''}
-        {selectedProject?.artDirection
-          ? (
-            <li>
-              Art direction,
-              {' '}
-              {selectedProject?.artDirection}
-            </li>
-          ) : ''}
-        {selectedProject?.focus
-          ? (
-            <li>
-              Focus puller,
-              {' '}
-              {selectedProject?.focus}
-            </li>
-          ) : ''}
-        {selectedProject?.camAssistant
-          ? (
-            <li>
-              Camera assistant,
-              {' '}
-              {selectedProject?.camAssistant}
-            </li>
-          ) : ''}
-        {selectedProject?.artAssistant
-          ? (
-            <li>
-              Art assistant,
-              {' '}
-              {selectedProject?.artAssistant}
-            </li>
-          ) : ''}
-        {selectedProject?.producer
-          ? (
-            <li>
-              Production,
-              {' '}
-              {selectedProject?.producer}
-            </li>
-          ) : ''}
-        {selectedProject?.productionAssistant
-          ? (
-            <li>
-              Production assistant,
-              {' '}
-              {selectedProject?.productionAssistant}
-            </li>
-          ) : ''}
-        {selectedProject?.car
-          ? (
-            <li>
-              Car by,
-              {' '}
-              {selectedProject?.car}
-            </li>
-          ) : ''}
-        {selectedProject?.music
-          ? (
-            <li>
-              Musci,
-              {' '}
-              {selectedProject?.music}
-            </li>
-          ) : ''}
-        {selectedProject?.color
-          ? (
-            <li>
-              Color grading,
-              {' '}
-              {selectedProject?.color}
-            </li>
-          ) : ''}
-        {selectedProject?.graphicDesigner
-          ? (
-            <li>
-              Graphic designer,
-              {' '}
-              {selectedProject?.graphicDesigner}
-            </li>
-          ) : ''}
-        {selectedProject?.compositing
-          ? (
-            <li>
-              Compositing,
-              {' '}
-              {selectedProject?.compositing}
-            </li>
-          ) : ''}
-        {selectedProject?.matte
-          ? (
-            <li>
-              Matte Painting,
-              {' '}
-              {selectedProject?.matte}
-            </li>
-          ) : ''}
-        {selectedProject?.editing
-          ? (
-            <li>
-              Editing,
-              {' '}
-              {selectedProject?.editing}
-            </li>
-          ) : ''}
-        {selectedProject?.artSupplier
-          ? (
-            <li>
-              Production assistant,
-              {' '}
-              {selectedProject?.artSupplier}
-            </li>
-          ) : ''}
-        {selectedProject?.coming
-          ? (
-            <li>
-              {selectedProject?.coming}
-            </li>
-          ) : ''}
-      </ul>
     </section>
   );
 };
