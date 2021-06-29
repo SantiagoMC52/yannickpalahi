ee750da5243bba3e73c5c99aed677faa280f0a0e
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-06-29 14:39:28 +0200

    Add information in about me component

--- src/components/Aboutme/index.jsx
+++ src/components/Aboutme/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './aboutme.scss';
 
 const Aboutme = () => (
-  <h2>About me</h2>
+  <section className="aboutme-container">
+    <div className="aboutme-container__text">
+      Hi, I’m Yannick and I was born in Girona in 1998. I studied a degree in
+      Audiovisual Communication and Multimedia of the University of Girona
+      (ERAM). Now, I’m currently working in art department for music videos,
+      short films and advertisments.
+    </div>
+  </section>
 );
 
 export default Aboutme;
