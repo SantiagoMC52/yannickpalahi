3fc3c04cca335b577ea1ed6e1e4cc957260cc6ca
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-06-30 21:18:59 +0200

    Correct about me text

--- src/components/Aboutme/index.jsx
+++ src/components/Aboutme/index.jsx
@@ -6,8 +6,8 @@ const Aboutme = () => (
     <div className="aboutme-container__text">
       Hi, I’m Yannick and I was born in Girona in 1998. I studied a degree in
       Audiovisual Communication and Multimedia of the University of Girona
-      (ERAM). Now, I’m currently working in art department for music videos,
-      short films and advertisments.
+      (ERAM). Now, I’m currently working at the art department for music
+      videos, short films and advertisments.
     </div>
   </section>
 );
