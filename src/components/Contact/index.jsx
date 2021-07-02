4b40b965a0025bde6e7616284bbf8584c02b7423
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-07-02 16:44:17 +0200

    Add info to contact component

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import './contact.scss';
 
 const Contact = () => (
-  <h2>Contact</h2>
+
+  <section className="contact-container">
+    <div className="contact-container__text">
+      <h2>get in touch</h2>
+      <h4>let`s work together, say hello</h4>
+    </div>
+
+    <div className="contact-container__media">
+      <p>657050235</p>
+      <p>yannickpalahi@gmail.com</p>
+      <a href="https://vimeo.com/user110474883" target="_blank" rel="noreferrer">vimeo</a>
+      <a href="https://www.instagram.com/ypalahi/" target="_blank" rel="noreferrer">instagram</a>
+    </div>
+  </section>
+
 );
 
 export default Contact;
