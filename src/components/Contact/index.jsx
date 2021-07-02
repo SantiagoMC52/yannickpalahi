08f4270ef60ac79a42976e52d1793b1d9abf761e
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-07-02 20:06:10 +0200

    Add styles to header in contact component

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,21 +2,21 @@ import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import { Divide as Hamburger } from 'hamburger-react';
-import '../Header/header.scss';
 import './contact.scss';
+import contactHeader from '../Header/header.module.scss';
 
 const Contact = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
     <>
-      <div className="top-container">
-        <Hamburger toggled={isOpen} toggle={setOpen} duration={0.8} color="#000000" />
+      <div className={contactHeader.container}>
+        <Hamburger toggled={isOpen} toggle={setOpen} duration={0.8} color="#ffffff" />
       </div>
-      <div className="navbar-container">
+      <div className={contactHeader.navbar}>
         {
         isOpen ? (
-          <nav className="navbar-container__nav">
+          <nav className={contactHeader.nav}>
             <Link to="/">YP</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/aboutme">About</Link>
@@ -26,11 +26,11 @@ const Contact = () => {
       }
       </div>
 
-      <div className="desktop-navbar">
-        <nav className="desktop-navbar__initials">
+      <div className={contactHeader.desktop}>
+        <nav className={contactHeader.initials}>
           <Link to="/">YP</Link>
         </nav>
-        <nav className="desktop-navbar__nav">
+        <nav className={contactHeader.nav}>
           <Link to="/projects">Projects</Link>
           <Link to="/aboutme">About</Link>
           <Link to="/contact">Contact</Link>
