38cb89e4ef310014c42fdc7da4e8215e55e2d9e7
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-07-02 17:21:09 +0200

    Change background color of contact component with react-helmet

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './contact.scss';
+import { Helmet } from 'react-helmet';
 
 const Contact = () => (
 
   <section className="contact-container">
+    <Helmet>
+      <style>{'body { background-color: black; color: white }'}</style>
+    </Helmet>
     <div className="contact-container__text">
       <h2>get in touch</h2>
       <h4>let`s work together, say hello</h4>
