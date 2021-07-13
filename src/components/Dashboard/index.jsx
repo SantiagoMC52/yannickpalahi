e51bcafc01a08413ffc87dbab9d4f276674f1b2f
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-07-13 17:12:18 +0200

    Add background video in home page

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './dashboard.scss';
 import Header from '../Header';
+import dashboardVideo from '../../assets/intro.mp4';
 
 const Dashboard = () => (
   <>
+    <video
+      autoPlay
+      loop
+      muted
+      className="intro"
+    >
+      <source src={dashboardVideo} type="video/mp4" />
+    </video>
     <Header />
     <section className="dashboard-container">
 
