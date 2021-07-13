d8488c4ddafd680cef6537c3b6734a69fbf0f9be
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-07-13 17:52:23 +0200

    Add navbar and inf changes in home page

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,30 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './dashboard.scss';
-import Header from '../Header';
+import { Link, NavLink } from 'react-router-dom';
+import { Divide as Hamburger } from 'hamburger-react';
 import dashboardVideo from '../../assets/intro.mp4';
+import contactHeader from '../Header/header.module.scss';
 
-const Dashboard = () => (
-  <>
-    <video
-      autoPlay
-      loop
-      muted
-      className="intro"
-    >
-      <source src={dashboardVideo} type="video/mp4" />
-    </video>
-    <Header />
-    <section className="dashboard-container">
+const Dashboard = () => {
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <>
+      <video
+        autoPlay
+        loop
+        muted
+        className="intro"
+      >
+        <source src={dashboardVideo} type="video/mp4" />
+      </video>
 
-      <div className="dashboard-container__name">
-        <p>yannick palahí</p>
+      <div className={contactHeader.desktop}>
+        <nav className={contactHeader.initials}>
+          <Link to="/">YP</Link>
+        </nav>
+        <nav className={contactHeader.nav}>
+          <Link to="/projects">projects</Link>
+          <Link to="/aboutme">about</Link>
+          <NavLink
+            exact
+            activeStyle={{ opacity: 0.5 }}
+            to="/contact"
+          >
+            contact
+
+          </NavLink>
+        </nav>
       </div>
 
-      <div className="dashboard-container__extra">
-        <p>art department</p>
+      <div className="top-container">
+        <Hamburger toggled={isOpen} toggle={setOpen} duration={0.8} color="#000000" />
+      </div>
+      <div className="navbar-container">
+        {
+        isOpen ? (
+          <nav className="navbar-container__nav">
+            <NavLink to="/">YP</NavLink>
+            <NavLink to="/projects">projects</NavLink>
+            <NavLink to="/aboutme">about</NavLink>
+            <NavLink to="/contact">contact</NavLink>
+          </nav>
+        ) : ''
+      }
       </div>
 
-    </section>
-  </>
-);
+      <section className="dashboard-container">
+
+        <p className="dashboard-container__year">2021</p>
+        <div className="dashboard-container__name">
+          <p>yannick palahí</p>
+        </div>
+
+        <div className="dashboard-container__extra">
+          <p>art department</p>
+        </div>
+
+      </section>
+    </>
+  );
+};
 export default Dashboard;
